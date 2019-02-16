2e64caf82f11d557e9cb1ca48f4e803888dc509d
Kevin Van Lierde <kevin.van.lierde@gmail.com>
2019-02-16 18:40:06 +0100

    Finalize thumbnail article, remove comments, tweak build process

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 #!/usr/bin/env node
 
-var Metalsmith = require('metalsmith');
-var collections = require('metalsmith-collections');
-var layouts = require('metalsmith-layouts');
-var markdown = require('metalsmith-markdown');
-var permalinks = require('metalsmith-permalinks');
-var drafts = require('metalsmith-drafts');
-var domTransform = require('metalsmith-dom-transform');
-var branch = require('metalsmith-branch');
-var sitemap = require('metalsmith-sitemap');
-var each = require('metalsmith-each');
-var serve = require('metalsmith-serve');
-var wordcount = require('metalsmith-word-count');
-var debugUI = require('metalsmith-debug-ui');
-var sass = require('metalsmith-sass');
-var slug = require('metalsmith-slug');
-var debug = require('metalsmith-debug');
-var minifyHTML = require('metalsmith-html-minifier');
-var uglify = require('metalsmith-uglify');
-var categories = require('./metalsmith/metalsmith-categories');
-var devrant = require('./metalsmith/metalsmith-devrant');
-var data = require('./metalsmith/metalsmith-data');
-var ignore = require('metalsmith-ignore');
-var admin = require('metalsmith-ui');
+var Metalsmith = require("metalsmith");
+var collections = require("metalsmith-collections");
+var layouts = require("metalsmith-layouts");
+var markdown = require("metalsmith-markdown");
+var permalinks = require("metalsmith-permalinks");
+var drafts = require("metalsmith-drafts");
+var domTransform = require("metalsmith-dom-transform");
+var branch = require("metalsmith-branch");
+var sitemap = require("metalsmith-sitemap");
+var each = require("metalsmith-each");
+var serve = require("metalsmith-serve");
+var wordcount = require("metalsmith-word-count");
+var debugUI = require("metalsmith-debug-ui");
+var sass = require("metalsmith-sass");
+var slug = require("metalsmith-slug");
+var debug = require("metalsmith-debug");
+var minifyHTML = require("metalsmith-html-minifier");
+var uglify = require("metalsmith-uglify");
+var categories = require("./metalsmith/metalsmith-categories");
+var devrant = require("./metalsmith/metalsmith-devrant");
+var data = require("./metalsmith/metalsmith-data");
+var ignore = require("metalsmith-ignore");
+//var admin = require("metalsmith-ui");
 
 var metalsmith = Metalsmith(__dirname);
-var isDev = process.argv[2] === 'serve'
+var isDev = process.argv[2] === "serve";
+var isPublish = process.argv[2] === "publish";
+var findArg = function(arg) {
+  var args = Array.prototype.slice.call(process.argv, 2);
+  return args.indexOf(arg) > -1;
+}
 
 if (isDev) {
-  metalsmith.use(admin());
+  metalsmith.use(serve({ port: 3000 }));
   /* metalsmith.use(serve({
     port: 4000,
     host: 'localhost',
@@ -38,76 +43,80 @@ if (isDev) {
 
 metalsmith
   // specify source directory
-  .source('./src')
-  // specify destination directory       
-  .destination('./dist')
+  .source("./src")
+  // specify destination directory
+  .destination("./dist")
   // clean destination directory
   .clean(true)
-  .use(devrant('webketje', isDev ? true : false))
+  .use(devrant("webketje", isPublish && findArg('--devrant') ? false : true )
   .use(debug())
   .metadata({
-    author: 'Kevin Van Lierde',
-    sitename: '\'t Webketje',
+    author: "Kevin Van Lierde",
+    sitename: "'t Webketje",
     siteurl: "https://webketje.com",
     description: "It's about saying »Hello« to the world.",
     generatorname: "Metalsmith",
     generatorurl: "http://metalsmith.io/"
   })
-  .use(sass({ outputDir: 'assets/css/' }))
-  .use(data('_data/*.json'))
+  .use(sass({ outputDir: "assets/css/" }))
+  .use(data("_data/*.json"))
   .use(drafts())
   .use(markdown())
   .use(wordcount())
-  .use(ignore(isDev ? [] : [
-    '_drafts/**/*'
-  ]))
-  .use(categories({
-    layout: 'pages/topic.twig',
-    category: 'topic',
-    contents: Buffer.from('')
-  }))
-  .use(collections({
-    posts: ['posts/**/*']
-  }))
-  .use(slug({
-    patterns: ['**/*.html'],
-    lower: true
-  }))
-  .use(permalinks({
-    relative: false,
-    linksets: [
-      {
-        match: { collection: 'posts' },
-        pattern: ':category/:title'
-      },
-      {
-        match: { type: 'topic'},
-        pattern: ':category/:title'
-      }
-    ]
-  }))
-  .use(layouts({
-    directory: './src/_layouts',
-    default: 'default.twig',
-    pattern: '**/*.html'
-  }))
+  .use(ignore(isDev ? [] : ["_drafts/**/*"]))
   .use(
-    ignore([
-      '_layouts/**/*',
-      '.devrant__cache'
-    ])
+    categories({
+      layout: "pages/topic.twig",
+      category: "topic",
+      contents: Buffer.from("")
+    })
   )
+  .use(
+    collections({
+      posts: ["posts/**/*"]
+    })
+  )
+  .use(
+    slug({
+      patterns: ["**/*.html"],
+      lower: true
+    })
+  )
+  .use(
+    permalinks({
+      relative: false,
+      linksets: [
+        {
+          match: { collection: "posts" },
+          pattern: ":category/:title"
+        },
+        {
+          match: { type: "topic" },
+          pattern: ":category/:title"
+        }
+      ]
+    })
+  )
+  .use(
+    layouts({
+      directory: "./src/_layouts",
+      default: "default.twig",
+      pattern: "**/*.html"
+    })
+  )
+  .use(ignore(["_layouts/**/*", ".devrant__cache"]))
   .use(uglify())
   .use(minifyHTML())
-  .use(sitemap({
-    hostname: 'https://webketje.com',
-    pattern: ['**/*.html'],
-    omitIndex: true,
-    changefreq: 'monthly',
-    lastmod: new Date()
-  }))
- // .use(debugUI.report('Permalinks'))
-  .build(function (err) {
+  .use(
+    sitemap({
+      hostname: "https://webketje.com",
+      pattern: ["**/*.html"],
+      omitIndex: true,
+      changefreq: "monthly",
+      lastmod: new Date()
+    })
+  )
+  // .use(debugUI.report('Permalinks'))
+  .build(function(err) {
     if (err) throw err;
   });
-
